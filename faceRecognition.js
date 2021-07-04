ef7f332678dab5dadbbeb0546cebd7e933027eb2
Surfsol <surfsol@protonmail.com>
2021-07-04 06:29:11 -0300

    update face with paxico

--- faceRecognition.js
+++ faceRecognition.js
@@ -8,53 +8,65 @@ const faceapi = require('@vladmandic/face-api');
 const router = express.Router();
 
 async function run(avatar, REFERENCE_IMAGE, profileId) {
-  console.log('in function run', avatar, REFERENCE_IMAGE)
+  console.log('inside run function', avatar)
   await faceDetectionNet.loadFromDisk("./models");
   await faceapi.nets.faceLandmark68Net.loadFromDisk("./models");
   await faceapi.nets.faceRecognitionNet.loadFromDisk("./models");
+  console.log("modals loaded")
+  let match
 
     let QUERY_IMAGE = avatar;
     const referenceImage = await canvas.loadImage(REFERENCE_IMAGE); 
     const queryImage = await canvas.loadImage(QUERY_IMAGE);
-   console.log({queryImage, referenceImage})
-    // detect faces
-    const resultsRef = await faceapi
+console.log('loaded on canvas')
+    // detect selfie face
+    const detectFaceRef = await faceapi
       .detectAllFaces(referenceImage, faceDetectionOptions)
       .withFaceLandmarks()
       .withFaceDescriptors();
-    console.log({resultsRef})
-    const resultsQuery = await faceapi
+    console.log({detectFaceRef})
+
+    if (detectFaceRef.length == 0) {
+        // no face
+        return match = false
+      }
+    console.log('detected selfie face')
+    // detect avatar face
+    const detectFaceAvatar = await faceapi
       .detectAllFaces(queryImage, faceDetectionOptions)
       .withFaceLandmarks()
       .withFaceDescriptors();
-    console.log({resultsQuery})
-    const faceMatcher = new faceapi.FaceMatcher(resultsRef, 0.55);
-    console.log({faceMatcher})
 
-    const labels = faceMatcher.labeledDescriptors.map((ld) => ld.label);
-
-    resultsQuery.map((res) => {
-      const bestMatch = faceMatcher.findBestMatch(res.descriptor);
-      if (bestMatch._label === "person 1") {
-        console.log('its a match')
-      } else {
-        console.log('no match')
+      if (detectFaceAvatar.length == 0) {
+        // no face
+       return match = false
       }
-    });
-  return ;
+        // indicates error, the lower more strict
+        const faceMatcher = new faceapi.FaceMatcher(detectFaceRef, 0.55);
+        console.log('detected avatar face')
+        detectFaceAvatar.map((res) => {
+            // compare selfie and avatar
+          const bestMatch = faceMatcher.findBestMatch(res.descriptor);
+          if (bestMatch._label === "person 1") {
+            return match = true
+          } else {
+            return match = false
+          }
+        });
+        console.log('compared avatar and selfie')
+  return match;
 }
-//run("https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/Thor/2.jpg", "https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/Hawkeye/1.jpg")
+
 router.post("/face", async (req, res) => {
-  
   const obj = req.body;
-  console.log(obj)
   const REFERENCE_IMAGE = obj.objImages.selfieUrl;
   const avatar = obj.objImages.avatar
   const profileId = obj.profileId
-  console.log({avatar, REFERENCE_IMAGE, profileId})
+  console.log("body info")
   try {
     const newArray = await run(avatar, REFERENCE_IMAGE, profileId);
-    if (newArray) {
+    console.log('response after run function', newArray)
+    if (res.status(200)) {
       res.status(200).json(newArray);
     }
   } catch (err) {
